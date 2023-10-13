7b748c084d866b14a8a48d8b6a7cf72a2ab905c7
Ali Livan Türk <88203714+Livanturk@users.noreply.github.com>
2023-10-13 13:00:50 +0300

    Update logs_controller.js

--- components/logs/logs_controller.js
+++ components/logs/logs_controller.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
+
 function log_data_reading() {
     const file_path = path.join(__dirname, 'log_data.json')
     try {
@@ -14,6 +15,12 @@ function log_data_reading() {
 
 const log_data = log_data_reading()
 
+function add_log(req, res){
+    const log_entry = req.body
+    log_data.push(log_entry)
+    res.status(201).json(log_entry)
+}
+
 function list_logs(req, res){
     const log_type = req.query.type
     if (log_type) {
@@ -26,15 +33,20 @@ function list_logs(req, res){
 
 function create_report(req, res) {
     const log_type = req.query.type
+    let filtered_logs = log_data
     if (log_type) {
-        const filtered_logs = log_data.filter((log) => log.type === log_type);
-        res.json({ report: filtered_logs });
-    } else {
-        res.json({ report: log_data });
+        filtered_logs = log_data.filter((log) => log.type === log_type)
     }
+    const create_report = filtered_logs.map((log) =>`Type: ${log.type}, Message: ${log.message}`)
+        .join('\n')
+    res.set('Content-Type', 'text/plain')
+    res.send(create_report)
 }
+
+
 
 module.exports = {
     list_logs,
     create_report,
+    add_log,
 }
