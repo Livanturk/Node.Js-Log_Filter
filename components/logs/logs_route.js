bb96c2a4af3791ecf8749dc8c18df584522cd363
Ali Livan Türk <88203714+Livanturk@users.noreply.github.com>
2023-10-13 20:10:30 +0300

    Update logs_route.js

--- components/logs/logs_route.js
+++ components/logs/logs_route.js
@@ -5,6 +5,9 @@ const logs_controller = require('./logs_controller')
 router.get('/logs',logs_controller.list_logs)
 router.get('/logs',logs_controller.create_report)
 router.post('/logs',logs_controller.add_log)
+router.get('/view-reports', (req, res) => {
+    res.sendFile(path.join(__dirname, 'reports.html'));
+});
 
 module.exports = {
     router
